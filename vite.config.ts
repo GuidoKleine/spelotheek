6adfa3243956b78474b7c1148257a6aa69c3e55d
Guido Kleine <guidokleine1991@gmail.com>
2025-11-24 15:28:52 +0100

    starting new project to learn more about react

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    reactRouter()
+  ],
+})
