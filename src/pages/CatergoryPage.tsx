1540346538550aa302c7220ee19aa8e330c1d194
Guido Kleine <guidokleine1991@gmail.com>
2026-09-16 16:46:47 +0200

    adjusted some styling, put text in volunteer page

--- src/pages/CatergoryPage.tsx
+++ src/pages/CatergoryPage.tsx
@@ -1,9 +1,8 @@
 import BaseCard from '../components/Catalogus/BaseCard';
-import '../styling/pages/CatalogusPage.css';
-import catalogusData from '../../test/mocks/stubs/catalogusData.json';
+import catagorieData from '../../test/mocks/stubs/catagorieData.json';
 
 function CatalogusPage() {
-  const CatalogusList = catalogusData.map((categorie) => (
+  const CatalogusList = catagorieData.map((categorie) => (
     <a href='.'>
       <BaseCard
         key={categorie.title}
@@ -15,15 +14,15 @@ function CatalogusPage() {
 
   return (
     <>
-      <section className="catalogus-page">
-        <h2>Catalogus</h2>
+      <div className="catalogus-page">
+        <h2>Catalogus Pagina</h2>
         <p className="catalogus-intro-block">
           Hier vindt u de complete catalogus van Speelotheek Woerden. Mail ons
           met je aanvragen als je iets zou willen reserveren kosten zijn €0,50
           per artikelen.
         </p>
         <div className="catalogus-grid-container">{CatalogusList}</div>
-      </section>
+      </div>
     </>
   );
 }
--- src/pages/VolunteerPage.tsx
+++ src/pages/VolunteerPage.tsx
@@ -1,7 +1,43 @@
+import '../styling/pages/VolunteerPage.css'
+
 function VolunteerPage() {
   return (
     <>
-      <h2>Vrijwilligers pagina</h2>
+    <section className="intro-block">
+      <h2>Vrijwilliger worden?</h2>
+      <p>
+        Heb jij een paar uurtjes per week of maand over en wil je graag een
+        steentje bijdragen aan een leuke, sociale en duurzame initiatief? Dan
+        zijn wij op zoek naar jou! Bij onze speelotheek zijn we dringend op zoek
+        naar <b>vrijwilligers</b> die ons team willen versterken. Ook voor je
+        maatschappelijke stage! Jij kunt ons helpen met:
+      </p>
+      <ul className="volunteer-list">
+        <li>
+          Werken aan de balie – Ontvang bezoekers, geef advies en verwerk
+          uitleningen.
+        </li>
+        <li>Repareren van speelgoed – Maak kapot speelgoed weer als nieuw!</li>
+        <li>
+          Uitleenbaar maken van speelgoed – Controleer en maak het speelgoed
+          schoon voor de volgende lener.
+        </li>
+      </ul>
+      <h4>Wat bieden wij?</h4>
+      <ul className="volunteer-list">
+        <li>Een gezellige werkomgeving met leuke collega’s.</li>
+        <li>
+          Een betekenisvolle bijdrage aan een duurzaam en sociaal project.
+        </li>
+        <li>
+          Vrijwillgers krijgen hun abonnement gratis en mogen speelgoed lenen
+          tot een bedrag van 5 euro per keer dat ze dienst hebben.
+        </li>
+      </ul>
+      <h4>Interesse?</h4>
+      <p>Laat het ons weten! Stuur een berichtje naar ons emailadres of kom langs tijdens onze openingstijden. We kijken uit naar je reactie!</p>
+      <img src="../../assets/vrijwilligers_aanmeld_form.jpg" role="img" aria-label="plaatje van posters voor vrijwillegers" alt="vrijwilligers poster"></img>
+      </section>
     </>
   );
 }
